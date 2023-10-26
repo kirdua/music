f50d238d12de8d221d6b879709c002554535f9b8
kirdua <kirdua@gmail.com>
2023-10-26 12:55:28 -0500

    added i18n

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -149,7 +149,7 @@ export default {
         class="w-4 h-4 float-left -ml-6 mt-1 rounded"
       />
       <i18n-t class="inline-block" keypath="register.accept" tag="label">
-        <a href="#">{{ $t("register.tos") }}</a>
+        <a href="#">Terms of Service</a>
       </i18n-t>
       <ErrorMessage class="text-red-600 block" name="tos" />
     </div>
